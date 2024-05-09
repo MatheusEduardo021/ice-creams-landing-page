b43ca63c51d4e1bbb6271cbe9937112fd1f09ca0
MatheusEduardo021 <matheusdevloper.2001@gmail.com>
2024-05-09 16:22:01 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+ScrollReveal().reveal('.nav-sr', {
+    duration: 1500,
+    distance: '50px',
+    origin: 'top',
+});
+
+ScrollReveal().reveal('.main-sr', {
+    duration: 1500,
+    distance: '150px',
+    origin: 'left',
+});
+
+
+ScrollReveal().reveal('.cardapio-sr', {
+    duration: 1500,
+    distance: '150px',
+    origin: 'left',
+});
+
+ScrollReveal().reveal('.img-sr', {
+    duration: 1500,
+    distance: '150px',
+    origin: 'left',
+});
+
+ScrollReveal().reveal('.testimonials-sr', {
+    duration: 1500,
+    distance: '150px',
+    origin: 'left',
+});
+
+
