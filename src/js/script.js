03132c077d8fdbdf6d848228dbf342210276ee37
MatheusEduardo021 <matheusdevloper.2001@gmail.com>
2024-05-11 20:37:27 -0300

    add X in nav-menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,50 @@
-ScrollReveal().reveal('.nav-sr', {
-    duration: 1500,
-    distance: '50px',
-    origin: 'top',
+ScrollReveal().reveal(".nav-sr", {
+  duration: 1500,
+  distance: "50px",
+  origin: "top",
 });
 
-ScrollReveal().reveal('.main-sr', {
-    duration: 1500,
-    distance: '150px',
-    origin: 'left',
+ScrollReveal().reveal(".main-sr", {
+  duration: 1500,
+  distance: "150px",
+  origin: "left",
 });
 
-
-ScrollReveal().reveal('.cardapio-sr', {
-    duration: 1500,
-    distance: '150px',
-    origin: 'left',
+ScrollReveal().reveal(".cardapio-sr", {
+  duration: 1500,
+  distance: "150px",
+  origin: "left",
 });
 
-ScrollReveal().reveal('.img-sr', {
-    duration: 1500,
-    distance: '150px',
-    origin: 'left',
+ScrollReveal().reveal(".img-sr", {
+  duration: 1500,
+  distance: "150px",
+  origin: "left",
 });
 
-ScrollReveal().reveal('.testimonials-sr', {
-    duration: 1500,
-    distance: '150px',
-    origin: 'left',
+ScrollReveal().reveal(".testimonials-sr", {
+  duration: 1500,
+  distance: "150px",
+  origin: "left",
 });
 
-const menuBtn = document.querySelector('#mobile-btn').addEventListener('click', menuShow)
+const menuBtn = document.querySelector("#mobile-btn");
+menuBtn.addEventListener("click", () => {
+  let menuMobile = document.querySelector(".mobile-menu");
+  let defaultIcon = document.querySelector("#mobile-btn");
 
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu')
+  if (menuMobile.classList.contains("actived")) {
+    menuMobile.classList.remove("actived");
+  } else {
+    menuMobile.classList.add("actived");
+  }
 
-    if(menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-    } else {
-        menuMobile.classList.add('open')
-    }
+  if (defaultIcon.className === "fa-solid fa-bars") {
+    defaultIcon.className = "fa-solid fa-x";
+  } else {
+    defaultIcon.className = "fa-solid fa-bars";
+  }
 
+});
 
-}
-
-// "fa-solid fa-x"> ---> X 
+// "fa-solid fa-x"> ---> X
